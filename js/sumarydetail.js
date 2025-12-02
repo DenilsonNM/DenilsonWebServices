240877cd2c0bf14237ec94cd9ea2dc13073fc9f9
DenilsonNM <denilsonjnm31@gmail.com>
2025-12-01 18:01:24 -0600

    Refactor FAQ section and enhance toggle functionality in servicios.html and sumarydetail.js

--- js/sumarydetail.js
+++ js/sumarydetail.js
@@ -1,3 +1,17 @@
+// $(".div_sumary").click(function () {
+//   $(this).next("ul").slideToggle();
+// });
+
 $(".div_sumary").click(function () {
-  $(this).next("ul").slideToggle();
+  const icon = $(this).find("i");
+  const content = $(this).next("ul");
+
+  content.slideToggle(400, function () {
+
+    if (content.is(":visible")) {
+      icon.removeClass("fa-caret-down").addClass("fa-caret-up");
+    } else {
+      icon.removeClass("fa-caret-up").addClass("fa-caret-down");
+    }
+  });
 });
